33aefef72a14245fa3d3991384a9550bec7db520
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-06-18 15:48:02 +0700

    feat: add context

--- src/basic_chat/index.ts
+++ src/basic_chat/index.ts
@@ -2,22 +2,32 @@ import OpenAI from "openai"
 
 const openai = new OpenAI({})
 
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: "system",
+    content: "You are a helpful chatbot.",
+  },
+]
+
+const createChatCompletion = async () => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: context,
+  })
+
+  const responseMessage = response.choices[0].message
+  context.push(responseMessage)
+
+  console.log(`${responseMessage.role}: ${responseMessage.content}\n`)
+}
+
 process.stdin.addListener("data", async (input) => {
   const userInput = input.toString().trim()
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o",
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful chatbot.",
-      },
-      {
-        role: "user",
-        content: userInput,
-      },
-    ],
+  context.push({
+    role: "user",
+    content: userInput,
   })
 
-  console.log(response.choices[0].message.content + "\n")
+  await createChatCompletion()
 })
